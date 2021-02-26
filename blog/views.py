c01ba5a0de4cf79c1adbac80ff2d8cd8a4c11d3e
lgsp_Harold <lgsp_leslie@126.com>
2021-02-26 21:16:38 +0800

    使用bootstrap优化布局及样式
    Signed-off-by: lgsp_Harold <lgsp_leslie@126.com>

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,10 @@ from blog.models import Blog, BlogType
 
 
 def blog_list(request):
-    context = {'blogs': Blog.objects.all()}
+    context = {
+        'blogs': Blog.objects.all(),
+        'blog_types': BlogType.objects.all(),
+    }
     return render(request, 'blog_list.html', context)
 
 
@@ -19,5 +22,6 @@ def blogs_with_type(request, blog_type_pk):
     context = {
         'blogs': Blog.objects.filter(blog_type=blog_type),
         'blog_type': blog_type,
+        'blog_types': BlogType.objects.all(),
     }
     return render(request, 'blog_with_type.html', context)
